e847b76ba5e39402c8861b18687e8e7bad1b5303
Scorch <lars.cornelissen02@gmail.com>
2021-05-05 15:06:14 +0200

    ScorchAIv2 with compilations

--- YoutubeAPI.py
+++ YoutubeAPI.py
@@ -1,34 +1,36 @@
 import datetime
-from Google import Create_Service
+from assets.Google import Create_Service
 from googleapiclient.http import MediaFileUpload
 import os
 import sys
-from assets import constants
 
 class YoutubeAPI:
-    def __init__(self): 
-        self.SERVICE = Create_Service(constants.CLIENTSECRETS_FILE, constants.API_NAME, constants.API_VERSION, constants.SCOPES)
+    def __init__(self, clientSecretsFile): 
+        self.SERVICE = Create_Service(clientSecretsFile, 'youtube', 'v3', ['https://www.googleapis.com/auth/youtube.upload'])
 
-    def uploadVideo(self, file, title, description, tags, uploadDate = datetime.datetime.now()):
+    def uploadVideo(self, file, title, description, tags, uploadDate = datetime.datetime.now(), categoryID = 20, notifySubscribers = True):
         request_body = {
             'snippet': {
                 'title': title,
                 'description': description,
                 'tags': tags,
-                'categoryId': 20
+                'categoryId': categoryID
             },
             'status': self.generateStatus(uploadDate),
-            'notifySubscribers': True
+            'notifySubscribers': notifySubscribers
         }
         self.insert(request_body, file)
 
     def generateStatus(self, uploadDate, forKids = False):
-         status = constants.VIDEO_STATUS
+         status = {
+            'privacyStatus': 'private',
+            'publishAt': "THIS SHOULD NOT BE PRINTED",
+            'selfDeclaredMadeForKids': False
+        }
          status["publishAt"] = uploadDate.isoformat()
          return status
 
     def insert(self, request_body, file):
-        print(file)
         mediaFile = MediaFileUpload(file)
         response_upload = self.SERVICE.videos().insert(
             part='snippet,status',
