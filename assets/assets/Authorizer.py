2bf249c5e7aa3b9a146eda2170855bfe67554ae9
Scorch <lars.cornelissen02@gmail.com>
2021-05-17 20:50:28 +0200

    Changed clips range from 1 day to 7

---
+++ assets/assets/Authorizer.py
@@ -0,0 +1,24 @@
+import json
+
+class Authorizer:
+    OAUTH = ""
+    CLIENT_ID = ""
+    AUTH_DATA = ""
+
+    def __init__(self, authDataFile): 
+        self.AUTH_DATA = self.readAuthFile(authDataFile)
+        self.OAUTH = self.getTOAUTHFromAuthData(self.AUTH_DATA)
+        self.CLIENT_ID = self.getClientIdFromAuthData(self.AUTH_DATA)
+    
+    def getHeaders(self):
+        return {"Authorization": self.OAUTH, "Client-Id": self.CLIENT_ID}
+        
+    def getClientIdFromAuthData(self, authData):  
+        return authData['twitch']['client-id']
+
+    def getTOAUTHFromAuthData(self, authData): 
+        return authData['twitch']['OAUTH']
+
+    def readAuthFile(self, authDataFile):
+        with open(authDataFile) as authData:
+            return json.load(authData)
