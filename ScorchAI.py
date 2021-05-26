03b9e2771f7440c872b5c648c27047b38836fd8a
Scorch <lars.cornelissen02@gmail.com>
2021-05-26 13:41:52 +0200

    Added Compilation system

--- ScorchAI.py
+++ ScorchAI.py
@@ -1,6 +1,7 @@
 from Youtube import Youtube
 from Twitch import Twitch
 import argparse
+import os
 
 VERSION = 2.2
 
@@ -8,13 +9,63 @@ parser = argparse.ArgumentParser(prog='scorchai', description='Uses scorchai to 
 
 parser.add_argument('-g', '--generate', action='store_true', help='Generate when no clip is found, doesnt do anything on its own')
 parser.add_argument('-u', '--upload', action='store_true', help='Upload clip')
+parser.add_argument('-c', '--compile', type=int, help='Compile clips', default=0)
 parser.add_argument('-v', '--version', action='version', version='%(prog)s {}'.format(VERSION))
 
 args = parser.parse_args()
 
-youtube = Youtube()
-twitch = Twitch()
+class ScorchAI:
+    def __init__(self, args): 
+        self.youtube = Youtube()
+        self.twitch = Twitch()
+
+    def runAI(self):
+        if args.compile > 0:
+            self.compile(args.compile)
+        if args.upload:  
+            self.youtube.uploadClip(args.generate)
+    
+    def compile(self, compileAmount):
+        self.setupCompile(compileAmount)
+        os.system("ffmpeg -y -f concat -safe 0 -i ./videos/prepstage/input.txt -c copy ./videos/uploaded_clips/output.mp4")
+        self.postCompile()
 
 
-if args.upload:   
-    youtube.uploadClip(args.generate)
+    def postCompile(self):
+        self.twitch.cleanFolder('./videos/clips/')
+        self.twitch.cleanFolder('./videos/prepstage/')
+        self.uploadCompilation()
+    
+    def uploadCompilation(self):
+        video = "./videos/uploaded_clips/output.mp4"
+        title = "AUTOMATED COMPILATION TEST"
+        tags = ""
+        with open("./assets/description.txt", encoding="utf8") as file:
+            description = file.read()
+        self.youtube.uploadVideo(video, title, description, tags)
+
+    
+    def setupCompile(self, amount):
+        vidAmount = len(self.getVideos('./videos/clips/'))
+        self.twitch.generateClips(amount - vidAmount)
+
+        a = open("./videos/prepstage/input.txt", "w")
+        for path, subdirs, files in os.walk('./videos/clips/'):
+            for filename in files:
+                if filename.endswith(".mp4"):
+                    os.system("ffmpeg -y -i ./videos/clips/{} -filter:v fps=60 -vcodec libx264 -c:a aac -preset ultrafast ./videos/prepstage/{}".format(filename, filename))
+                    a.write("file {}\n".format(filename)) 
+        a.close()
+
+    def getVideos(self, folder):
+        videos = os.listdir(folder)
+        for i in range(len(videos)):
+            videos[i] = videos[i].split(".mp4")[0]
+        return videos
+
+
+
+
+scai = ScorchAI(args)
+scai.runAI()
+ 
